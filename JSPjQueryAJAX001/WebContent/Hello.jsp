bdff382d0f9b88ce8a8fb5395500711eac4e6a57
AlexanderAgopov <alexander.agopov@gmail.com>
2019-02-06 01:02:55 +0200

    Add JSP, jQuery and AJAX exercise

---
+++ JSPjQueryAJAX001/WebContent/Hello.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>jQuery, AJAX and Servlet/JSP example</title>
+<script src="jquery-3.3.1.min.js" type="text/javascript"></script>
+<script src="ajax.js" type="text/javascript"></script>
+</head>
+<body>
+	<form>
+		Enter your username: <input type="text" id="userName" />
+	</form>
+	<br>
+	<br>
+	<strong>AJAX Response</strong>
+	<div id="ajaxGetUserServletResponse"></div>
+</body>
+</html>
---
+++ JSPjQueryAJAX001/src/servlets/GetUserServlet.java
@@ -0,0 +1,40 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetUserServlet
+ */
+@WebServlet("/GetUserServlet")
+public class GetUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public GetUserServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("userName").trim();
+		if (username == null || username.equals("")) {
+			username = "Guest";
+		}
+		String greetings = "Hello " + username + "!";
+		response.setContentType("text/plain");
+		response.getWriter().write(greetings);
+	}
+
+}
